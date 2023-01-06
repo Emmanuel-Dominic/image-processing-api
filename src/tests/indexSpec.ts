af006444e9603818e69796d6dedd69e62fc32c70
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2023-01-06 19:48:44 +0300

    chore: implement unittesting

---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,22 @@
+import express, { Express, Request, Response } from 'express';
+import request from "supertest";
+import app from '../index';
+
+let server: Express;
+
+beforeEach(() => {
+    server = app;
+});
+
+describe("When an Image Processing endpoint is accessed.", () => {
+    it('should be able to return status code of 200.', () => {
+        request(server).get("http://localhost:3000/encenadaport.jpg", (err, res) => {
+            expect(res.status).toBe(200);
+        });
+    });
+    it('should be able to return an image name in the body.', () => {
+        request(server).get("http://localhost:3000/encenadaport.jpg", (err, res) => {
+            expect(res.body).toContain('encenadaport.jpg');
+        });
+    });
+});
